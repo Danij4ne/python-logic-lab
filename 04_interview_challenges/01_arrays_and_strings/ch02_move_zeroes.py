f12cf18c8137dd88e164bc3ee5832cc9095d5b62
Daniel Jane <danij4ne@gmail.com>
2025-11-27 10:21:43 +0100

    new exercise created offline

---
+++ 04_interview_challenges/01_arrays_and_strings/ch02_move_zeroes.py
@@ -0,0 +1,32 @@
+
+"""
+CH02 - Move Zeroes
+
+Description:
+    Given a list of numbers, move all zeroes to the end of the list
+    while keeping the order of the non-zero elements the same.
+
+Requirements:
+    - Create a list of numbers (hard-coded or user input).
+    - Move all elements equal to 0 to the end.
+    - Keep the relative order of non-zero elements.
+    - Print the list before and after the transformation.
+
+Notes:
+    - Try to solve it without creating a completely new list
+      (in-place modification is preferred, but not mandatory).
+    - This is a common array manipulation interview question.
+
+Example:
+    Input:
+    [0, 1, 0, 3, 12]
+
+    Output:
+    [1, 3, 12, 0, 0]
+"""
+
+# TODO:
+# 1. Create a list that contains some zeroes.
+# 2. Move all zeroes to the end of the list.
+# 3. Keep the order of the non-zero elements.
+# 4. Print the list before and after the change.
