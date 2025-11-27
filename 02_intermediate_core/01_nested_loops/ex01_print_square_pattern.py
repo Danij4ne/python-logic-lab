9a805ed8a42af0ee566b364d3f5ca4b9ec247fda
Daniel Jane <danij4ne@gmail.com>
2025-11-27 10:17:15 +0100

    new exercise created offline

---
+++ 02_intermediate_core/01_nested_loops/ex01_print_square_pattern.py
@@ -0,0 +1,32 @@
+
+"""
+Ex01 - Print Square Pattern
+
+Description:
+    Write a program that asks the user for a number N
+    and prints an N x N square made of a chosen character.
+    Use nested loops to generate the pattern.
+
+Requirements:
+    - Ask the user for a number N (size of the square).
+    - Ask the user for a character or symbol.
+    - Use nested loops (a loop inside another loop).
+    - Print an N x N square using the chosen character.
+
+Example:
+    Enter size:
+    4
+    Enter character:
+    *
+
+    ****
+    ****
+    ****
+    ****
+"""
+
+# TODO:
+# 1. Ask the user for the square size (N).
+# 2. Ask the user for a character.
+# 3. Use nested loops to print an N x N square.
+
