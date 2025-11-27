10bf69b6a5a9986d378610f630a93d52fc9a1b2e
Daniel Jane <danij4ne@gmail.com>
2025-11-27 10:15:41 +0100

    new exercise created offline

---
+++ 01_beginner_basics/01_variables_input_output/ex01_hello_name.py
@@ -0,0 +1,30 @@
+
+"""
+Ex01 - Hello Name
+
+Description:
+    Write a program that asks the user for their name
+    and prints a personalized greeting.
+
+Requirements:
+    - Ask the user for their name using input().
+    - Store the value in a variable.
+    - Print a message that includes the name.
+
+Example:
+    Enter your name:
+    Daniel
+
+    Hello Daniel, welcome!
+"""
+
+
+
+
+
+
+# TODO:
+# 1. Ask the user for their name using input().
+# 2. Store the name in a variable.
+# 3. Print a greeting message that includes the name.
+
