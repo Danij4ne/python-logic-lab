040f01f157a002cc50edc539564f14315a8ba148
Daniel Jane <danij4ne@gmail.com>
2025-11-27 10:17:20 +0100

    new exercise created offline

---
+++ 02_intermediate_core/01_nested_loops/ex02_multiplication_table_matrix.py
@@ -0,0 +1,29 @@
+
+"""
+Ex02 - Multiplication Table Matrix
+
+Description:
+    Write a program that asks the user for a number N
+    and prints an N x N multiplication table using nested loops.
+
+Requirements:
+    - Ask the user for a number N.
+    - Convert the input to an integer.
+    - Use nested loops to generate the table.
+    - Each cell should contain (row_index * column_index).
+    - Print the table in a clear grid-like format.
+
+Example:
+    Enter size:
+    3
+
+    1  2  3
+    2  4  6
+    3  6  9
+"""
+
+# TODO:
+# 1. Ask the user for the table size (N).
+# 2. Convert the input to an integer.
+# 3. Use nested loops to generate the multiplication table.
+# 4. Print each row on a separate line.
