c957508fb2624e592f3ad5bb1185ce8a9349d431
Daniel Jane <danij4ne@gmail.com>
2026-02-08 12:59:04 +0100

    Challenge merge two sorted lists completed

--- 04_interview_challenges/02_linked_lists_and_custom_structs/ch03_merge_two_sorted_lists.py
+++ 04_interview_challenges/02_linked_lists_and_custom_structs/ch03_merge_two_sorted_lists.py
@@ -34,3 +34,97 @@ Example (expected behavior):
 # 2. Implement merge_sorted_lists(list1, list2).
 # 3. Create two sorted lists and merge them.
 # 4. Print the merged result.
+
+ 
+
+# ---------- Node ----------
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# ---------- LinkedList ----------
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+
+        self.tail.next = new_node
+        self.tail = new_node
+
+    def print_list(self):
+        current = self.head
+        while current is not None:
+            print(current.value, end=" -> ")
+            current = current.next
+        print("None")
+
+
+# ---------- Merge function ----------
+def merge_sorted_lists(head1, head2):
+    dummy = Node(0)
+    tail = dummy
+
+    a = head1
+    b = head2
+
+    while a is not None and b is not None:
+        if a.value <= b.value:
+            tail.next = a
+            a = a.next
+        else:
+            tail.next = b
+            b = b.next
+
+        tail = tail.next
+
+    # uno de los dos aún puede tener nodos
+    if a is not None:
+        tail.next = a
+    else:
+        tail.next = b
+
+    return dummy.next
+
+
+ 
+list1 = LinkedList()
+list1.add(1)
+list1.add(3)
+list1.add(5)
+
+list2 = LinkedList()
+list2.add(2)
+list2.add(4)
+list2.add(6)
+
+print("List 1:")
+list1.print_list()
+
+print("List 2:")
+list2.print_list()
+
+merged_head = merge_sorted_lists(list1.head, list2.head)
+
+merged_list = LinkedList()
+merged_list.head = merged_head
+
+print("Merged:")
+merged_list.print_list()
+
+
+
+
+
+
+
+        
